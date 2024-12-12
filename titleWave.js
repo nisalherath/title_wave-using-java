4fb9a86d8bb92a82cb417a7b6f3d858c665c550e
nisalherath <anushkanisalherath@gmail.com>
2024-12-12 10:53:03 +0530

    Updated titleWave.js with improvements

--- titleWave.js
+++ titleWave.js
@@ -1,47 +1,31 @@
 function waveTitleEffect() {
-    // The base title to display at all times
     var baseTitle = "Nisal";
-    // The additional text to "wave" into the title
     var waveText = "Herath";
-    // Combined full title to work with
     var title = baseTitle + waveText;
-    // Holds the current title being displayed in the wave effect
     var waveTitle = title;
-    // Index tracker for the current position in the wave effect
     var i = 0;
-    // Direction tracker: true for forward, false for backward
     var forward = true;
-
-    // Function to update the title for the wave effect
+  
     function updateTitle() {
         if (forward) {
-            // Expanding the title by one character at a time
             if (i < title.length) {
-                // Display the substring of the title up to the current index
                 document.title = waveTitle.substring(0, i + 1);
-                i++; // Move the index forward
+                i++;
             } else {
-                // Reverse direction once the full title is displayed
                 forward = false;
-                i--; // Step back one character
+                i--;
             }
         } else {
-            // Shrinking the title back down to the base title
             if (i >= baseTitle.length) {
-                // Display the substring of the title up to the current index
                 document.title = waveTitle.substring(0, i);
-                i--; // Move the index backward
+                i--;
             } else {
-                // Reverse direction once the base title is reached
                 forward = true;
-                i++; // Start expanding again
+                i++;
             }
         }
     }
-
-    // Set an interval to repeatedly update the title every 400ms
+  
     setInterval(updateTitle, 400);
 }
-
-// Start the title wave effect
 waveTitleEffect();
