2356d522f3687cc5eb6e86a5a2fe0bcfbecd683a
nisalherath <anushkanisalherath@gmail.com>
2024-12-12 10:51:40 +0530

    Updated titleWave.js with improvements

--- titleWave.js
+++ titleWave.js
@@ -1,31 +1,32 @@
 function waveTitleEffect() {
-    var baseTitle = "Nisal";
-    var waveText = "Herath";
-    var title = baseTitle + waveText;
+    var baseTitle = "Nisal"; // The constant part of the title
+    var waveText = "Herath"; // The text that animates in the wave effect
+    var title = baseTitle + waveText; // Full title to work with
     var waveTitle = title;
-    var i = 0;
-    var forward = true;
-  
+    var i = 0; // Tracks the current position in the animation
+    var forward = true; // Direction of animation: true for forward, false for backward
+
     function updateTitle() {
         if (forward) {
             if (i < title.length) {
-                document.title = waveTitle.substring(0, i + 1);
+                document.title = waveTitle.substring(0, i + 1); // Expand the title
                 i++;
             } else {
-                forward = false;
+                forward = false; // Reverse direction when full title is reached
                 i--;
             }
         } else {
             if (i >= baseTitle.length) {
-                document.title = waveTitle.substring(0, i);
+                document.title = waveTitle.substring(0, i); // Shrink the title
                 i--;
             } else {
-                forward = true;
+                forward = true; // Reverse direction when base title is reached
                 i++;
             }
         }
     }
-  
-    setInterval(updateTitle, 400);
+
+    setInterval(updateTitle, 400); // Updates the title every 400ms
 }
-waveTitleEffect();
+
+waveTitleEffect(); // Starts the title wave effect
